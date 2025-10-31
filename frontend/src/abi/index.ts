6521e747233cf1a9625926394b6ea120c39a36b9
loserner <loserner@proton.me>
2025-10-31 12:44:44 +0100

    Copy index.ts

---
+++ frontend/src/abi/index.ts
@@ -0,0 +1,4 @@
+export { default as addresses } from './addresses.json';
+export { default as TransactionAnalyzer } from './TransactionAnalyzer.json';
+export { default as FHEVMPrivateAnalyzer } from './FHEVMPrivateAnalyzer.json';
+export { default as FHEVMImplementation } from './FHEVMImplementation.json';
